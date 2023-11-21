df6f061b89b357319f778add4e15fd0f7a3d8d96
Skye <102645867+skylarposler@users.noreply.github.com>
2023-11-20 19:07:41 -0800

    Microservice
    Incorporated microservice

--- pokepicker.py
+++ pokepicker.py
@@ -41,9 +41,57 @@ def get_pokemon():
 
     pokemon_menu(pokemon_details)
 
+def get_pokemon_micro():
+    """Generates pokemon name for microservice"""
+
+    randomize_dex = random.randint(0, 898) # generate random index
+    location = "C:/Users/Tiana/Documents/Skye's Stuff/School/cs361/Assignment 6/pokedex.json"
+    dex =  open(location, encoding="utf-8")
+    data = json.load(dex)
+
+    # select random pokemon
+    counter = 0
+    for idx in data:
+        counter += 1 
+        if counter == randomize_dex:
+            name = data[counter]['name']
+            pokemon = name['english']
+            pokemon_name = json.dumps(pokemon)
+
+    return pokemon_name
+
+
 def find_gen(id):
     """Locates the generation of the chose pokemon"""
     # to implement later
+
+def run_microservice():
+    """writes to microservice file"""
+    
+    while True:
+        time.sleep(1)
+        file = open("microservice.txt", 'w')
+        file.write("request")
+        file.close()
+        time.sleep(1)
+
+        file = open("microservice.txt", 'r+')
+        line = file.readline()
+        file.close()
+        time.sleep(1)
+
+        if line == "request":
+            pokemon = get_pokemon_micro() # calls the microservice
+            outfile = open("microservice.txt", 'w')
+            poke_name = pokemon.strip('"')
+            outfile.write(poke_name)
+            outfile.close()
+            break
+        else:
+            print("Microservice Error.")
+
+    return poke_name
+
 
 
 def pokemon_menu(pokemon_details):
@@ -53,7 +101,7 @@ def pokemon_menu(pokemon_details):
 
     print("Your pokemon is ", pokemon_details['Pokemon'], "\n Type: ", pokemon_details['Type'], "\n Level: ", pokemon_details['Level'])
     
-    pokemon_menu_select = input("Pokemon Menu:\nSelect 1 to View Pokemon Details\n Select 2 to Add to your Party\n Select 3 to return to the Main Menu\n")
+    pokemon_menu_select = input("Pokemon Menu:\nSelect 1 to View Pokemon Details\n Select 2 to Add to your Party\n Select 3 to show this pokemon's favorite Food\nSelect 4 to return to the Main Menu\n")
 
     time.sleep(1)
     if pokemon_menu_select == "1":
@@ -80,7 +128,12 @@ def pokemon_menu(pokemon_details):
             print(pokemon_details['Pokemon'], "has been added to your Party.\n")
             time.sleep(1)
             default_menu()
-    if pokemon_menu_select == "3":
+
+        # if pokemon_menu_select == "3":
+
+        #     # to implement later
+
+    if pokemon_menu_select == "4":
         checker = input("If you return to the Main Menu, this pokemon may be lost. Return anyways? Select 1 for Yes, 2 for No.\n")
         if checker == "1":
             time.sleep(1)
@@ -134,4 +187,4 @@ def default_menu():
             print("That is not a vaild menu option, please try again.\n")
             default_menu()
 
-default_menu()
+run_microservice()
